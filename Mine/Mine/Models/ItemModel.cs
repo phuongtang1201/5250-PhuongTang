f95fd635b2b7611a79e58dec6eac8864da2b1823
phuongtang1201 <mtang1@seattleu.edu>
2022-01-16 18:21:41 -0800

    Added comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-
+/// <summary>
+/// Items for the Characters and Monsters to use
+/// </summary>
 namespace Mine.Models
 {
     public class ItemModel
     {
+        //The Id for the item
         public string Id { get; set; }
+
+        //The Display Text for the item
         public string Text { get; set; }
+
+        //The Description for the item
         public string Description { get; set; }
 
         //The value of the Item +9 Damage
