2e5cb18aa97a638d0617a0b76a7006d8b48c563e
phuongtang1201 <mtang1@seattleu.edu>
2022-01-22 14:57:09 -0800

    Renamed AddItemAsync to CreateAsync in MockDataStore.cs, IDataStore.cs, ItemIndexViewModel.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -23,7 +23,7 @@ namespace Mine.Services
             };
         }
 
-        public async Task<bool> AddItemAsync(ItemModel item)
+        public async Task<bool> CreateAsync(ItemModel item)
         {
             items.Add(item);
 
