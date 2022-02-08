8b11090536e72fd258f7b6c73caa1951ad0d83b0
phuongtang1201 <mtang1@seattleu.edu>
2022-02-07 19:24:06 -0800

    Added RollDice_Valid_Roll_2_Dice_6 in DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -40,6 +40,20 @@ namespace UnitTests.Helpers
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result <= 6);
         }
+        [Test]
+        public void RollDice_Valid_Roll_2_Dice_6_Should_Return_Between_2_And_12()
+        {
+            //Arrange
+
+            //Act
+            var result = DiceHelper.RollDice(2, 6);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result >= 2);
+            Assert.AreEqual(true, result <= 12);
+        }
 
         [Test]
         public void RollDice_Valid_Roll_1_Dice_6_Should_Not_Less_1_Greater_6()
