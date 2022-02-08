f99c6b143afee70953532ccf0ba533cc93cacd8a
phuongtang1201 <mtang1@seattleu.edu>
2022-02-07 18:23:30 -0800

    Added DiceHelper.cs to project

---
+++ Mine/Mine/Helpers/DiceHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mine.Helpers
+{
+    public static class DiceHelper
+    {
+        private static Random md = new Random();
+        public static bool ForceRollsToNotRandom = false;
+        public static int ForceRandomValue = 1;
+        public static int RollDice (int rolls, int dice)
+        {
+            if (ForceRollsToNotRandom)
+            {
+                return rolls * ForceRandomValue;
+            }
+            if (rolls < 1)
+            {
+                return 0;
+            }
+            if(dice < 1)
+            {
+                return 0;
+            }
+            var myReturn = 0;
+            for (var i = 0; i< rolls; i++)
+            {
+                myReturn += md.Next(1, dice + 1);
+            }
+            return myReturn;
+        }
+    }
+}
