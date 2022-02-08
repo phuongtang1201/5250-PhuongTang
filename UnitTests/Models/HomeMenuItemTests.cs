82b51b6ba09b7f560c01579158e4a248a885872d
phuongtang1201 <mtang1@seattleu.edu>
2022-02-07 18:15:53 -0800

    Added HomeMenyItem_Set_Get UT to HomeMenuItemTests.cs

--- UnitTests/Models/HomeMenuItemTests.cs
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -19,6 +19,25 @@ namespace UnitTests.Models
             //Assert
             Assert.IsNotNull(result);
         }
-  
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            //Arange
+
+
+            //Act
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.About;
+            result.Title = "Title";
+           
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(MenuItemType.About, result.Id);
+            Assert.AreEqual("Title", result.Title);
+
+        }
+
     }
 }
