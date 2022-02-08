8f41cb0e413a39dc78da08f2ee1203c2cd8f8d4d
phuongtang1201 <mtang1@seattleu.edu>
2022-02-07 17:44:41 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid()
+        {
+            //Arange
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
